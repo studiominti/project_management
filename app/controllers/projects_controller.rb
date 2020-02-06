3f44426b1eac7daadd763d4ec95ffa6f3256eff2
dev-mint <studiominti@gmail.com>
2020-02-06 00:07:23 -0300

    Create bootstrap navigation and dashboard

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -61,6 +61,9 @@ class ProjectsController < ApplicationController
     end
   end
 
+  def filter
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_project
