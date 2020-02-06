9ff091a6a70e01801b7e91b4e2fa1cb8af1a6bdb
dev-mint <studiominti@gmail.com>
2020-02-05 23:26:00 -0300

    Projects scaffold and migration

---
+++ db/migrate/20200206022046_create_projects.rb
@@ -0,0 +1,13 @@
+class CreateProjects < ActiveRecord::Migration[5.2]
+  def change
+    create_table :projects do |t|
+      t.string :name
+      t.text :description
+      t.string :start_date
+      t.string :finish_date
+      t.string :status
+
+      t.timestamps
+    end
+  end
+end
